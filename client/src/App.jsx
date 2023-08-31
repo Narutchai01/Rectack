c97d289261735d2b2537c4af586ecab80ab2b4f7
tawayahc <yotachayawat@gmail.com>
2023-08-31 23:11:43 +0700

    test1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,6 +23,7 @@ function App() {
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
+          Halo, test test
         </p>
       </div>
       <p className="read-the-docs">
