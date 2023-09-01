7d3c6e208a9b552c6db03e1c7392088860515d7a
tawayahc <yotachayawat@gmail.com>
2023-09-02 00:01:56 +0700

    Fixed Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
                 <div onClick={ handleNav } className='block md:hidden'>
                     { !nav ? <AiOutlineClose size={ 20 } /> : <AiOutlineMenu size={ 20 } /> }
                 </div>
-                <div className={ nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-white ease-in-out duration-500' : 'fixed left-[-100%] ease-in-out duration-500' }>
+                <div className={ !nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-white ease-in-out duration-500' : 'fixed left-[-100%] ease-in-out duration-500' }>
                     <h1 className="w-full text-3xl font-bold text-[#CB160E] m-4">Rectack</h1>
                     <ul className='p-4'>
                         <li className="p-4 border-b border-gray-600">STACK</li>
