840bfc6d16e90fa01dc530bc5c487e02edda7d1c
Narutchai <koonfluk2015@gmail.com>
2023-09-01 22:17:40 +0700

    add router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,25 @@
-import React from "react"
+// import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import { Route, Routes } from "react-router-dom"
+import AboutPage from "./pages/AboutPage"
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Hero />
-    </div>
+    <>
+      <div>
+        <Navbar />
+        <Hero />
+      </div>
+
+
+
+      <Routes>
+        <Route path="/about" element={<AboutPage/>} />
+      </Routes>
+
+    </>
+
   )
 }
 
