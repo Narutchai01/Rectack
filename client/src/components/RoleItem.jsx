09c642139a42271452f43b9eb4602e9c2c2c8125
tawayahc <yotachayawat@gmail.com>
2023-09-02 16:51:25 +0700

    Added Role Section

--- client/src/components/RoleItem.jsx
+++ client/src/components/RoleItem.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const RoleItem = () => {
+const RoleItem = (props) => {
+  const { title } = props
+
   return (
-    <div className=''>
-        <button className=''>Web Development</button>
+    <div>
+        <button className='bg-white border-solid border border-[#8A8E99] text-[#231815] py-4 px-8 rounded-lg hover:bg-gradient-to-br from-[#CB160E] to-[#EA5B37] hover:text-white hover:border-hidden'>{ title }</button>
     </div>
   )
 }
--- client/src/pages/StackPage.jsx
+++ client/src/pages/StackPage.jsx
@@ -1,6 +1,18 @@
-import RoleItem from "../components/RoleItem"
+import { useState } from "react"
+import RoleSection from "../components/RoleSection"
 
 const StackPage = () => {
+    const RoleList = [
+        {id : 1, title : "Web Development"},
+        {id : 2, title : "Mobile Development"},
+        {id : 3, title : "Game Development"},
+        {id : 4, title : "Data Analysis"},
+        {id : 5, title : "Computer Vision"},
+        {id : 6, title : "NLP"}
+    ]
+
+    const [items, setItem] = useState(RoleList)
+
     return (
         <>
             <div className="max-w-[1240px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center items-center">
@@ -8,10 +20,8 @@ const StackPage = () => {
                 <p className="text-[#231815] text-2xl">Let’s find the right tools for your project</p>
                 <p className="text-[#231815] text-2xl"><span className="text-[#EA5B37]">Learn</span> tools | <span className="text-[#EA5B37]">Ask</span> question | <span className="text-[#EA5B37]">Guide</span> developer</p>
                 <div className="w-full border-b-2 my-7"></div>
-                <h1 className="text-[#231815] font-bold text-5xl">Select the Role</h1>
-                <div className="grid grid-cols-4 gap-4">
-                    <RoleItem />
-                </div>
+                <h1 className="text-[#231815] font-bold text-5xl pb-7">Select the Role</h1>
+                <RoleSection items = { items } />
             </div>
         </>
     )
