14b4add4a94f4724f5d2b75f104cfea5dd86c32b
FlukeSupakorn <supakorn.tungpa@gmail.com>
2023-09-16 21:25:30 +0700

    ChatbotV.1(Solvebug)

--- client/src/components/Chatbot/ChatBar.jsx
+++ client/src/components/Chatbot/ChatBar.jsx
@@ -11,20 +11,24 @@ const ChatBar = (props) => {
         setInputText(e.target.value);
     };
 
+    const handleSubmit = () => {
+        // Perform the same action as pressing Enter
+        handleKeyPress({ key: 'Enter' });
+    };
+
     const handleKeyPress = (e) => {
         if (e.key === 'Enter' && !e.shiftKey) {
         e.preventDefault();
-        console.log(inputText);
         setInputText('');
         setSubmitted(true);
-        setChatMessages([...chatMessages, inputText]);
+        setChatMessages([...chatMessages, inputText.replace(/\n/g, '<br/>')]);
         }
     };
 
     return (
         <> 
-            <div className={`justify-center ${submitted ? 'flex h-full' : 'hidden'}`}>
-                <div className='w-[60%]'>
+            <div className={`justify-center overflow-scroll ${submitted ? 'flex h-full' : 'hidden'}`}>
+                <div className='w-[60%] '>
                     <ChatFeed messages={chatMessages}/>
                 </div>
             </div>
@@ -36,18 +40,14 @@ const ChatBar = (props) => {
                 <div className='fixed bottom-0 bg-white h-[20vh] w-full'/>
                 <div className={`fixed w-1/2 h-20 ${submitted ? 'bottom-10' : ''}`}>
                     <textarea
-                        className="w-full h-20 pl-4 py-6 border border-gray-400 focus:outline-none focus:ring-1 focus:ring-red-300 rounded-lg resize-none mr-2"
+                        className="w-full h-20 pl-4 pr-16 py-6 border border-gray-400 focus:outline-none focus:ring-1 focus:border-red-400 focus:ring-red-300 rounded-lg resize-none mr-2"
                         placeholder="Ask anything..."
                         value={inputText}
                         onChange={handleInputChange}
                         onKeyDown={handleKeyPress}
                     ></textarea>
                     <button
-                        className="absolute right-4 top-[30%] text-gray-400 p-2"
-                        onClick={() => {
-                            console.log(inputText);
-                            setInputText('');
-                        }}
+                        className="absolute hover:text-red-300 focus:outline-none right-4 top-[30%] text-gray-400 p-2 rounded-full"
                     >
                         <IconArrowRightCircle height={20} width={20} />
                     </button>
--- client/src/components/Chatbot/ChatFeed.jsx
+++ client/src/components/Chatbot/ChatFeed.jsx
@@ -4,28 +4,29 @@ const ChatFeed = (props) => {
     const { messages } = props;
 
     const answers = [
-        "Hello!",
-        "How can I help you?",
-        "I'm here to assist you.",
-        "What's on your mind?",
-        "Ask me anything!",
+        "Hello!sdlkamdkakkajasdasddsfl;j;aldkjfl;asdadasdla/sdk;aksd;askd'fk'a;ldkf';akdf;akds';fka;'kdf;kasd'f;lak;ljasd;ljf;lakdsjfl;j;lasdfadfafadf",
+        "How can I help you? <br/> sdlkamdkakkajasdasddsfl;j;aldkjfl;asdadasdla/sdk;aksd;askd'fk'a;ldkf';akdf;akds';fka;'kdf;kasd'f;lak;ljasd;ljf;lakdsjfl;j;lasdfadfafadf",
+        "I'm here to assist you.sdlkamdkakkajasdasddsfl;j;aldkjfl;asdadasdla/sdk;aksd;askd'fk'a;ldkf';akdf;akds';fka;'kdf;kasd'f;lak;ljasd;ljf;lakdsjfl;j;lasdfadfafadf",
+        "What's on your mind?sdlkamdkakkajasdasddsfl;j;aldkjfl;asdadasdla/sdk;aksd;askd'fk'a;ldkf';akdf;akds';fka;'kdf;kasd'f;lak;ljasd;ljf;lakdsjfl;j;lasdfadfafadf",
+        "Ask me anything!sdlkamdkakkajasdasddsfl;j;aldkjfl;asdadasdla/sdk;aksd;askd'fk'a;ldkf';akdf;akds';fka;'kdf;kasd'f;lak;ljasd;ljf;lakdsjfl;j;lasdfadfafadf",
     ];
 
     return (
-        <div className="mx-10 mt-10 overflow-hidden">
-            <ul className="list-none pr-[15px] mr-[-15px] overflow-y-scroll max-h-[500px]">
+        <div className="pb-[15%] pt-10 mx-10 overflow-hidden">
+            <ul className="list-none">
                 {messages.map((message, index) => (
                     <li key={index} className='my-2'>
-                        <div className="flex items-center justify-end">
-                            <span className="inline-block p-2 mx-2 rounded-lg bg-[#EA5B37] text-white">
-                                {message}
+                        <div className="my-4 flex items-center justify-end">
+                            <span className="inline-block max-w-[50%] p-2 mx-2 rounded-lg bg-[#EA5B37] text-white break-all">
+                                <span dangerouslySetInnerHTML={{ __html: message}}/>
                             </span>
                             <span className='h-10 w-10 border-2 rounded-full'></span>
                         </div>
-                        <div className='my-2 flex items-center justify-start'>
+                        <div className='my-4 flex items-center justify-start'>
                             <span className='h-10 w-10 border-2 rounded-full'></span>
-                            <span className="inline-block p-2 mx-2 rounded-lg border">
-                                {answers[Math.floor(Math.random() * answers.length)]}
+                            <span className="inline-block max-w-[50%] p-2 mx-2 rounded-lg border break-all"
+                                dangerouslySetInnerHTML={{ __html: answers[Math.floor(Math.random() * answers.length)]}}
+                            >
                             </span>
                         </div>
                     </li>
