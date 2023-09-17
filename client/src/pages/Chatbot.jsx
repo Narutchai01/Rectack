6edb0d179eaa1a3e2d2c441cb2850b51305fcfe6
Narutchai <39022151+Narutchai01@users.noreply.github.com>
2023-09-17 20:29:58 +0700

    Update Chatbot.jsx

--- client/src/pages/Chatbot.jsx
+++ client/src/pages/Chatbot.jsx
@@ -1,12 +1,12 @@
 // import React from 'react'
 // import { Form } from 'react-router-dom'
-import ChatBar from '../components/Chatbot/Chatbar.jsx'
+import ChatBar from '../components/Chatbot/ChatBar'
 
 const Chatbot = () => {
     return (
         <>
             <div className='h-screen overflow-hidden'>
-                <ChatBar/>     
+                <ChatBar />
             </div>
         </>
     )
